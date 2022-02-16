cd22e0fe6571085c456ce806d861c3a8f39e661b
unknown <kristhian.segura@labinfo.is.escuelaing.edu.co>
2022-02-16 12:42:59 -0500

    Avance

--- src/main/java/URL/URLApp.java
+++ src/main/java/URL/URLApp.java
@@ -1,5 +1,9 @@
 package URL;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.*;
 import java.util.Scanner;
 
@@ -7,14 +11,17 @@ public class URLApp {
 
     // URL de prueba
     // https://www.escuelaing.edu.co:80/es?resource=ofertas/"
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println("Ingrese una URL:");
         Scanner sc = new Scanner(System.in);
         String input = sc.next();
         try {
             URL urlInput = new URL(input);
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(urlInput.openStream()));
         } catch (MalformedURLException e) {
             System.out.println("La URL ingresada está malformada");
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 }
